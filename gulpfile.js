062b5aac6e71e772a6d536d54125418d227eef37
Nicola Febbrari <febbrari@gmail.com>
2019-08-28 11:41:58 +0200

    update gulp

--- gulpfile.js
+++ gulpfile.js
@@ -1,20 +1,22 @@
 'use strict'
 
-var gulp = require('gulp');
+const {
+  src,
+  dest,
+  series
+} = require('gulp');
 var clean = require('gulp-clean');
-var jshint = require('gulp-jshint');
-var jscs = require('gulp-jscs');
 var concat = require('gulp-concat');
 var uglify = require('gulp-uglify');
 var header = require('gulp-header');
 
 var bases = {
- app: 'src/',
- dist: 'dist/',
+  app: 'src/',
+  dist: 'dist/',
 };
 
 var paths = {
- scripts: ['**/*.js']
+  scripts: ['**/*.js']
 };
 
 var pkg = require('./package.json');
@@ -25,31 +27,34 @@ var banner = ['/**',
   ' * @link <%= pkg.homepage %>',
   ' * @license <%= pkg.license %>',
   ' */',
-  ''].join('\n');
+  ''
+].join('\n');
 
 // Delete the dist directory
-gulp.task('clean', function() {
- return gulp.src(bases.dist)
- .pipe(clean());
-});
+const clear = function () {
+  return src(bases.dist)
+    .pipe(clean());
+};
 
 // Process scripts and concatenate them into one output file
-gulp.task('scripts', ['clean'], function() {
- gulp.src(paths.scripts, {cwd: bases.app})
- .pipe(jshint())
- .pipe(jshint.reporter('default'))
- .pipe(jscs())
- .pipe(jscs.reporter())
- .pipe(uglify())
- .pipe(concat('gridstack-angular.min.js'))
- .pipe(header(banner, { pkg : pkg } ))
- .pipe(gulp.dest(bases.dist));
+const min = () => src(paths.scripts, {
+    cwd: bases.app
+  })
+  .pipe(uglify())
+  .pipe(concat('gridstack-angular.min.js'))
+  .pipe(header(banner, {
+    pkg: pkg
+  }))
+  .pipe(dest(bases.dist));
+const full = () => src(paths.scripts, {
+    cwd: bases.app
+  })
+  .pipe(concat('gridstack-angular.js'))
+  .pipe(header(banner, {
+    pkg: pkg
+  }))
+  .pipe(dest(bases.dist));
 
- gulp.src(paths.scripts, {cwd: bases.app})
- .pipe(concat('gridstack-angular.js'))
- .pipe(header(banner, { pkg : pkg } ))
- .pipe(gulp.dest(bases.dist));
-});
 
-// Define the default task as a sequence of the above tasks
-gulp.task('default', ['clean', 'scripts']);
+
+exports.default = series(clear, min, full)
